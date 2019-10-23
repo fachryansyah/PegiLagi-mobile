d600ffaabb8ee8930d3c10a531f4ffb5b129d04e
Indra Juniyanto <indrajuniyanto96@gmail.com>
2019-10-23 22:41:10 +0700

    Create Screen Register

--- src/Navigation.js
+++ src/Navigation.js
@@ -14,6 +14,7 @@ import OrderAirPlaneTicket from './Screen/Order/OrderAirplaneTicket'
 import OrderTrainTicket from './Screen/Order/OrderTrainTicket'
 import OrderHotel from './Screen/Order/OrderHotel'
 import LoginScreen from './Screen/LoginScreen'
+import RegisterScreen from './Screen/RegisterScreen'
 import SearchStation from './Screen/Search/SearchStation'
 import SearchAirPort from './Screen/Search/SearchAirPort'
 
@@ -89,7 +90,10 @@ const MainNavigation = createStackNavigator(
 		},
 		Login: {
 			screen: LoginScreen
-    },
+		},
+		Register: {
+			screen: RegisterScreen
+		},
 		SearchStation: {
 			screen: SearchStation
 		},
